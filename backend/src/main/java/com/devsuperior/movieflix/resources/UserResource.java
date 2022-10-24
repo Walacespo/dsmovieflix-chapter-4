3c787253b100423f66d1d1cdb4c2f9df889ac85f
mackristofer <mackristofer@gmail.com>
2022-10-24 19:01:02 -0300

    Teste ok

--- backend/src/main/java/com/devsuperior/movieflix/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/movieflix/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.devsuperior.movieflix.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +17,8 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping(value = "/profile")
-	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
-		UserDTO dto = service.findById(id);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> getProfile() {
+		UserDTO dto = service.getProfile();
+		return ResponseEntity.ok(dto);
 	}
 }
